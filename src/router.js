18ea52a20105af774ec909130ed580b1004668ab
coder-zzk <2238109333@qq.com>
2019-01-24 16:54:33 +0800

    添加个人中心相关组件

--- src/router.js
+++ src/router.js
@@ -7,6 +7,15 @@ import bilibiliRead from '@/components/projects/bilibili-read'
 import baseArticle from '@/components/article/base-article'
 import articleView from '@/components/article/article-view'
 import articleWrite from '@/components/article/article-write'
+import baseUser from '@/components/user/base-user'
+import userInfo from '@/components/user/user-info'
+import userPassword from '@/components/user/user-password'
+import userSafe from '@/components/user/user-safe'
+import userComment from '@/components/user/user-comment'
+import userArtcile from '@/components/user/user-article'
+import userSubscribe from '@/components/user/user-subscribe'
+import userRecord from '@/components/user/user-record'
+import userPicture from '@/components/user/user-picture'
 Vue.use(Router)
 
 export default new Router({
@@ -51,6 +60,49 @@ export default new Router({
         path: 'view',
         name: 'article-view',
         component: articleView
+      },{
+        path:'/user',
+        name:'user',
+        component:baseUser,
+        children:[
+          {
+            path:'info',
+            name:'user-info',
+            component:userInfo
+          },
+          {
+            path:'password',
+            name:'user-password',
+            component:userPassword
+          },
+          {
+            path:'safe',
+            name:'user-safe',
+            component:userSafe
+          },
+          {
+            path:'comment',
+            name:'user-comment',
+            component:userComment
+          },
+          {
+            path:'article',
+            name:'user-article',
+            component:userArtcile
+          },{
+            path:'subscribe',
+            name:'user-subscribe',
+            component:userSubscribe
+          },{
+            path:'record',
+            name:'user-record',
+            component:userRecord
+          },{
+            path:'picture',
+            name:'user-picture',
+            component:userPicture
+          }
+        ]
       }]
     }
   ]
