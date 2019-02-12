5b3aa5d2b678d0881b1a082db80a6be7f551e168
coder-zzk <2238109333@qq.com>
2019-02-12 22:09:32 +0800

    将font-awe-some引入配置独立

--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import './plugins/font-awe-some.js'
 import router from './router'
 import store from './store'
-import fontawesome from '@fortawesome/fontawesome'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import solid from '@fortawesome/fontawesome-free-solid'
-import regular from '@fortawesome/fontawesome-free-regular'
-import brands from '@fortawesome/fontawesome-free-brands'
 
-fontawesome.library.add(solid)
-fontawesome.library.add(regular)
-fontawesome.library.add(brands)
-// 导入fontawesome
-Vue.component('icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 export default new Vue({
