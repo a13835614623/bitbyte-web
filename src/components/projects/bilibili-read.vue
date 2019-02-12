a1792ca050f534af064c800f17c474c498374020
coder-zzk <2238109333@qq.com>
2019-02-12 22:37:06 +0800

    echarts改为按需引入

--- src/components/projects/bilibili-read.vue
+++ src/components/projects/bilibili-read.vue
@@ -2,8 +2,13 @@
   <div class="bilibili-read"></div>
 </template>
 <script>
-// 引入echarts和axios
-import echarts from "echarts";
+// 引入 ECharts 主模块
+var echarts = require('echarts/lib/echarts');
+// 引入柱状图
+require('echarts/lib/chart/bar');
+// 引入提示框和标题组件
+require('echarts/lib/component/tooltip');
+require('echarts/lib/component/title');
 import axios from "axios";
 //初始化图表框架
 var newColor = function(index) {
