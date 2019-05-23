a39a5469e3a3edfb6b519e7836e13067f39d5243
coder-zzk <2238109333@qq.com>
2019-05-23 16:53:40 +0800

    add 添加分区属性映射PARTS_PROP_MAP #constant

--- src/util/constant.js
+++ src/util/constant.js
@@ -7,8 +7,18 @@ const ARTICLE_PARTS = [
   '技术前瞻',
   '其他',
 ];
+const PARTS_PROP_MAP = {
+  language:'编程语言',
+  development:'开发技术',
+  algorithm:'数据结构与算法',
+  database:'数据库',
+  architecture:'架构设计',
+  latest:'技术前瞻',
+  other:'其他',
+};
 module.exports={
-  ARTICLE_PARTS
+  ARTICLE_PARTS,
+  PARTS_PROP_MAP
 }
 // export default {
 //   ARTICLE_PARTS,
