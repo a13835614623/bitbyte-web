4867a0adaf9fdf3f24be4a16d2d008b64df46694
coder-zzk <2238109333@qq.com>
2020-04-10 11:33:38 +0800

    添加转发到QQ和QQ空间功能

--- src/components/article/article-view.vue
+++ src/components/article/article-view.vue
@@ -24,7 +24,7 @@
         <p v-html="article.articleContent"></p>
       </div>
       <div class="line"></div>
-      <!-- 点赞-收藏 -->
+      <!-- 点赞-收藏 -分享-->
       <el-row>
         <el-button type="danger"
                    :disabled="!isLogin"
@@ -41,6 +41,40 @@
           &nbsp;
           <span>{{isFavorite?"已收藏":"收藏"}}</span>
         </el-button>
+        <el-popover placement="right"
+                    width="150"
+                    style="margin-left:10px;"
+                    trigger="click">
+          <el-button slot="reference"
+                     type="primary">
+            <icon :icon="['far','share-square']" />
+            转发
+          </el-button>
+          <el-button type="primary"
+                     round
+                     @click="share(1)">
+            <icon :icon="['fab','qq']" />
+            分享到QQ
+          </el-button>
+          <el-button type="warning"
+                     round
+                     style="margin:10px 0 0;"
+                     @click="share(2)">
+            <svg t="1586485906986"
+                 class="icon"
+                 viewBox="0 0 1028 1024"
+                 version="1.1"
+                 xmlns="http://www.w3.org/2000/svg"
+                 p-id="1818"
+                 width="14"
+                 height="14">
+              <path d="M852.982272 693.76c-46.08 23.04-58.88 43.52-48.64 94.72 10.24 53.76 23.04 110.08 33.28 163.84 2.56 10.24 0 23.04 0 33.28-15.36-2.56-35.84 0-48.64-7.68-30.72-17.92-58.88-43.52-92.16-64-58.88-35.84-117.76-69.12-186.88-76.8-12.8-2.56-25.6 0-35.84 5.12-79.36 43.52-158.72 87.04-235.52 133.12-17.92 10.24-33.28 12.8-46.08 0-7.68-7.68-5.12-23.04-5.12-35.84 15.36-92.16 33.28-186.88 51.2-279.04 2.56-17.92 0-30.72-15.36-43.52-71.68-64-140.8-125.44-209.92-192-7.68-5.12-15.36-20.48-12.8-25.6 2.56-7.68 15.36-15.36 25.6-15.36 94.72-12.8 186.88-25.6 281.6-38.4 17.92-2.56 28.16-10.24 35.84-25.6 51.2-89.6 102.4-179.2 153.6-268.8 5.12-7.68 12.8-12.8 17.92-20.48 7.68 7.68 15.36 12.8 17.92 20.48 43.52 89.6 87.04 181.76 128 273.92 7.68 17.92 20.48 28.16 40.96 28.16 99.84 7.68 197.12 17.92 296.96 28.16 7.68 0 15.36 7.68 23.04 10.24-5.12 7.68-7.68 17.92-12.8 23.04-66.56 66.56-135.68 133.12-202.24 199.68-15.36 15.36-25.6 40.96-43.52 69.12-92.16-7.68-194.56-15.36-304.64-23.04 104.96-74.24 204.8-145.92 309.76-222.72-186.88-48.64-366.08-38.4-545.28-2.56 0 2.56 0 7.68 0 10.24 122.88 7.68 245.76 17.92 368.64 25.6 2.56 2.56 2.56 5.12 5.12 7.68-7.68 7.68-15.36 15.36-25.6 20.48-89.6 58.88-179.2 115.2-266.24 174.08-10.24 7.68-20.48 15.36-30.72 25.6C474.102272 734.72 663.542272 711.68 852.982272 693.76z"
+                    p-id="1819"
+                    fill="#FFCE00"></path>
+            </svg>
+            分享到QQ空间
+          </el-button>
+        </el-popover>
       </el-row>
       <!-- 作者 -->
       <el-row type="flex"
@@ -208,6 +242,7 @@ export default {
       favoriteGroupMap: {},
       favoriteGroupCheckList: [],
       isAddingFavoriteGroup: false,
+      shareObj: { title: "", desc: "", summary: "" },
       ARTICLE_PART_MAP
     };
   },
@@ -341,6 +376,39 @@ export default {
         this.$message.error("移除收藏失败!");
         console.error(error);
       }
+    },
+    share(type) {
+      let share = {
+        title: this.article.articleTitle,
+        url: window.location.href,
+        desc: "我在bitbyte博客看到一篇不错的文章,一起来看看吧!",
+        img: "http://106.13.32.222/img/logo.43bcb58f.png",
+        summary: this.article.articleTitle
+      };
+      let url = "";
+      switch (type) {
+        case 1:
+          url = this.qqShareURL(share);
+          break;
+        case 2:
+          url = this.qZoneShareURL(share);
+          break;
+        default:
+          url =""
+          this.$message.error("非法操作!");
+      }
+      if(url!=""){
+        console.log(url)
+        window.open(url,"_blank");
+      }
+    },
+    // 分享到QQ空间
+    qZoneShareURL({ title, desc, summary, url, img }) {
+      return `http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=${url}&title=${title}&desc=${desc}&summary=${summary}&site=bitbyte&pics=${img}`;
+    },
+    // 分享到QQ
+    qqShareURL({ title, summary, desc, img, url }) {
+      return `http://connect.qq.com/widget/shareqq/index.html?url=${url}&title=${title}&desc=${desc}&pics=${img}&summary=${summary}`;
     }
   },
   computed: {
