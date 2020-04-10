676005f9c3ab6d1c040216de0772eb0c7a28ceaa
coder-zzk <2238109333@qq.com>
2020-04-10 11:34:13 +0800

    添加分享和QQ图标

--- src/plugins/font-awe-some.js
+++ src/plugins/font-awe-some.js
@@ -7,7 +7,10 @@ import faUser from '@fortawesome/fontawesome-free-solid/faUser'
 import faUnlockAlt from '@fortawesome/fontawesome-free-solid/faUnlockAlt'
 import farThumbsUp from '@fortawesome/fontawesome-free-regular/faThumbsUp'
 import faStar from '@fortawesome/fontawesome-free-solid/faStar'
+import faShare from '@fortawesome/fontawesome-free-solid/faShare'
 import farStar from '@fortawesome/fontawesome-free-regular/faStar'
+import faShareSquare from '@fortawesome/fontawesome-free-regular/faShareSquare'
+import faQq from '@fortawesome/fontawesome-free-brands/faQq'
 
 library.add(faEdit)
 library.add(faUser)
@@ -16,7 +19,9 @@ library.add(faThumbsUp)
 library.add(farThumbsUp)
 library.add(faStar)
 library.add(farStar)
-
+library.add(faShare)
+library.add(faShareSquare)
+library.add(faQq)
 // import solid from '@fortawesome/fontawesome-free-solid'
 // import regular from '@fortawesome/fontawesome-free-regular'
 
