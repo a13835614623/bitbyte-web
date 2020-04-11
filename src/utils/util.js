51c9bd202e0dc8eef083851099ed56676d6f7fb2
coder-zzk <2238109333@qq.com>
2020-04-11 23:27:48 +0800

    ARTICLE_STATE_MAP添加DELETED：32已删除状态

--- src/utils/util.js
+++ src/utils/util.js
@@ -5,7 +5,7 @@ import {
   IS_LOGIN,
   GET_LOCAL_USER,
   GET_LOCAL_TOKEN,
-  SAVE_LOCAL_DATA,
+  SAVE_LOCAL_DATA
 } from './local';
 const ARTICLE_PARTS = [
   '编程语言',
@@ -14,7 +14,7 @@ const ARTICLE_PARTS = [
   '数据库',
   '架构设计',
   '技术前瞻',
-  '其他',
+  '其他'
 ];
 const ARTICLE_PART_MAP = {
   16: '编程语言',
@@ -23,19 +23,19 @@ const ARTICLE_PART_MAP = {
   19: '数据库',
   20: '架构设计',
   21: '技术前瞻',
-  22: '其他',
+  22: '其他'
 };
 // 访问类型属性k/v映射
 const ACCESS_TYPE_MAP = {
   14: '首页',
-  15: '博客分区',
+  15: '博客分区'
 };
 // 访问值属性k/v映射
 const ACCESS_VALUE_MAP = Object.assign(
   {
-    23: '首页',
+    23: '首页'
   },
-  ARTICLE_PART_MAP,
+  ARTICLE_PART_MAP
 );
 // 文章状态
 const ARTICLE_STATE_MAP = {
@@ -44,11 +44,13 @@ const ARTICLE_STATE_MAP = {
   27: '已创建',
   28: '已发布',
   29: '审核中',
-  PASS:24,
-  REFUSE:25,
-  CREATED:27,
-  PUBLISHED:28,
-  AUDITING:29
+  32: '已删除',
+  PASS: 24,
+  REFUSE: 25,
+  CREATED: 27,
+  PUBLISHED: 28,
+  AUDITING: 29,
+  DELETED: 32
 };
 export {
   ARTICLE_PARTS,
