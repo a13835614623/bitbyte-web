d8fd2acf24dd1644ce54fc49398250cb7c49bb99
coder-zzk <2238109333@qq.com>
2020-04-19 17:28:57 +0800

    添加背景，格式化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <div class="bg">
+    </div>
     <el-container>
       <!-- 导航 -->
       <el-header>
@@ -17,24 +19,24 @@
   </div>
 </template>
 <script>
-import navHeader from '@/components/nav-header';
-import pageFooter from '@/components/page-footer';
-import { WEBSOCKET_URL, GET_LOCAL_TOKEN } from '@/utils/util.js';
-import { mapState, mapMutations,mapGetters } from 'vuex';
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
+import navHeader from "@/components/nav-header";
+import pageFooter from "@/components/page-footer";
+import { WEBSOCKET_URL, GET_LOCAL_TOKEN } from "@/utils/util.js";
+import { mapState, mapMutations, mapGetters } from "vuex";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 export default {
   components: {
-    'nav-header': navHeader,
-    'page-footer': pageFooter,
+    "nav-header": navHeader,
+    "page-footer": pageFooter
   },
   computed: {
-    ...mapState(['user']),
-    ...mapGetters(['isLogin'])
+    ...mapState(["user"]),
+    ...mapGetters(["isLogin"])
   },
   data() {
     return {
-      stompClient: null,
+      stompClient: null
     };
   },
   created() {
@@ -44,15 +46,15 @@ export default {
   },
   mounted() {
     if (this.isLogin) this.initWebSocket();
-    else{
-      console.log("未登录，无法连接")
+    else {
+      console.log("未登录，无法连接");
     }
   },
   beforeDestroy() {
     this.disconnect();
   },
   methods: {
-    ...mapMutations(['SAVE_USER']),
+    ...mapMutations(["SAVE_USER"]),
     initWebSocket() {
       // 建立连接对象
       let socket = new SockJS(WEBSOCKET_URL);
@@ -60,7 +62,7 @@ export default {
       let headers = {
         Authorization: GET_LOCAL_TOKEN(),
         UserId: this.user.userId,
-        'content-type': 'application/json;charset=utf8;',
+        "content-type": "application/json;charset=utf8;"
       };
       // 获取STOMP子协议的客户端对象
       this.stompClient = Stomp.over(socket);
@@ -72,55 +74,55 @@ export default {
       this.stompClient.connect(
         headers,
         () => {
-          console.log('连接成功!');
+          console.log("连接成功!");
           this.subscribe(headers);
         },
         err => {
           // 连接发生错误时的处理函数
           // 订阅服务端提供的某个topic
           this.$notify({
-            title: '提示',
-            message: '连接异常！',
+            title: "提示",
+            message: "连接异常！",
             duration: 3000,
             offset: 100,
-            type: 'warning',
+            type: "warning"
           });
           console.error(err);
-        },
+        }
       );
     },
     notify({ type, message }) {
       this.$notify({
-        title: '提示',
+        title: "提示",
         message,
         duration: 6000,
         offset: 100,
-        type,
+        type
       });
     },
     subscribeMap() {
       let map = {
-        '/system/notice': msg => {
+        "/system/notice": msg => {
           let { noticeContent } = JSON.parse(msg.body);
           // 订阅服务端提供的某个topic
           this.notify({
             message: noticeContent,
-            type:"info",
+            type: "info"
           });
-        },
+        }
       };
-      map['/user/' + this.user.userId + '/audit/end'] = msg => {
-        console.log(msg.body)
+      map["/user/" + this.user.userId + "/audit/end"] = msg => {
+        console.log(msg.body);
         let { noticeContent } = JSON.parse(msg.body);
-        noticeContent=JSON.parse(noticeContent);
+        noticeContent = JSON.parse(noticeContent);
         let stateMap = {
-          24: '审核通过',
-          25: '审核被拒绝',
+          24: "审核通过",
+          25: "审核被拒绝"
         };
         this.notify({
           message: `您投递的博客《${noticeContent.articleTitle}》${
             stateMap[noticeContent.auditState]
-          }!`,
+          }!`
         });
       };
       return map;
@@ -138,25 +140,36 @@ export default {
       if (this.stompClient) {
         this.stompClient.disconnect(() => {
           this.$notify({
-            title: '提示',
-            message: '连接断开！',
+            title: "提示",
+            message: "连接断开！",
             duration: 3000,
             offset: 100,
-            type: 'warning',
+            type: "warning"
           });
         });
       }
-    }, // 断开连接
-  },
+    } // 断开连接
+  }
 };
 </script>
 <style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background-color: #f2f6fc;
+  /* background-color: #f2f6fc; */
   min-height: 800px;
+}
+.bg{
+  z-index: -1;
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background: url(~@/assets/img/carousel-2.jpg);
+  -webkit-filter: blur(10px); /* Chrome, Opera */
+  -moz-filter: blur(10px);
+  -ms-filter: blur(10px);
+  filter: blur(10px);
 }
 /* .el-main{
   height: 700px;
