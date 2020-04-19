e2b9d2b322139379c80af368a4cd855d3256df10
coder-zzk <2238109333@qq.com>
2020-04-19 17:28:29 +0800

    修复SAVE_USER的问题

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -10,7 +10,7 @@ export default {
   // 保存用户信息
   SAVE_USER(state, payload) {
     if (payload && payload.user) {
-      state.user = Object.assign(state.user,payload.user);
+      state.user = Object.assign(state.user||{},payload.user);
       if (!IS_LOGIN()) {
         SAVE_LOCAL_DATA(payload);
       }
