60e038ff2d4629ec7446774da01176a07f677e3c
coder-zzk <2238109333@qq.com>
2020-05-07 16:12:24 +0800

    添加文章图片路径

--- src/utils/url.js
+++ src/utils/url.js
@@ -2,4 +2,5 @@
 const BASE_URL = '/api';
 const WEBSOCKET_URL = BASE_URL+'/message/ws';
 const USER_PIC_PRE_URL = BASE_URL+'/user/pic/get?name=';
-export { WEBSOCKET_URL, BASE_URL, USER_PIC_PRE_URL };
+const ARTICLE_PIC_PRE_URL=BASE_URL+"/article/pic/get?name=";
+export { WEBSOCKET_URL, BASE_URL, USER_PIC_PRE_URL,ARTICLE_PIC_PRE_URL };
